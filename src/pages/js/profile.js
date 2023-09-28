09bb3f78932581b9c0c7bd58d092cae3a04c7e40
EmersonRabelo <emersonrabelo1998@gmail.com>
2023-09-28 18:58:31 -0300

    Fix: Logout system

--- src/pages/js/profile.js
+++ src/pages/js/profile.js
@@ -2,21 +2,21 @@ const menuOpt = document.querySelectorAll('.profile-menu_opt');
 
 
 // TODO: Corrigir bug do svg fill
-//[...menuOpt].forEach(element => {
-//    element.addEventListener('click', e => {
+[...menuOpt].forEach(element => {
+    element.addEventListener('click', e => {
 
-//        if(e.currentTarget.getAttribute('action') == 'out'){
-//            logOut()
-//        }
+        if(e.currentTarget.getAttribute('action') == 'out'){
+            logOut()
+        }
 
-//        const svg = e.currentTarget.childNodes[1].childNodes[1]
-//        const paths = svg.querySelectorAll('path');
+        //const svg = e.currentTarget.childNodes[1].childNodes[1]
+        //const paths = svg.querySelectorAll('path');
 
-//        paths.forEach(path => {
-//            path.style.fill = '#54BAB1'; // Substitua 'sua_cor_aqui' pela cor desejada.
-//        });
-//    })
-//});
+        //paths.forEach(path => {
+        //    path.style.fill = '#54BAB1'; // Substitua 'sua_cor_aqui' pela cor desejada.
+        //});
+    })
+});
 
 function logOut(){
     window.location.replace(`${window.location.origin}/src/pages/login.html`)
